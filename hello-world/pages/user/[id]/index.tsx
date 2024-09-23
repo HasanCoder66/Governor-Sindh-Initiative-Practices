357619fb87dd52ee553b5f7daa7f5685253d520b
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-23 20:09:17 +0500

    learn useSWR client side rendering

--- hello-world/pages/user/[id]/index.tsx
+++ hello-world/pages/user/[id]/index.tsx
@@ -1,54 +1,35 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import useSWR from 'swr';
 
 interface User {
   id: number;
   firstName: string;
   lastName: string;
-  image : string;
-  address : string;
-  city : string;
-
-  // Add other user properties as needed
+  image: string;
+  address: {
+    address: string;
+    city: string;
+  };
 }
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 const InfoUserPage: React.FC = () => {
-  const [userData, setUser] = useState<User | null>(null); // Start with null for a single user
   const router = useRouter();
   const { id } = router.query; // Destructure id from router.query
 
-  const fetchingUsersData = async (userId: string | string[] | undefined) => {
-    if (!userId) return; // Check if userId is available
+  const { data, error } = useSWR<User>(id ? `https://dummyjson.com/users/${id}` : null, fetcher);
 
-    try {
-      const response = await fetch(`https://dummyjson.com/users/${userId}`);
-      const data = await response.json(); // No need to type if it's a single user
-      console.log(data)
-      setUser(data); // Set the user directly
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    if (id) {
-      fetchingUsersData(id); // Call the fetch function with the id
-    }
-  }, [id]); // Run the effect when id changes
+  if (error) return <div>Error loading user data...</div>;
+  if (!data) return <div>Loading user data...</div>;
 
   return (
     <div>
       <h1>INFO USER PAGE</h1>
-      {userData ? (
-        <>
-          <h2>{userData?.firstName} {userData.lastName}</h2>
-          {/* <Image alt="Users Image" width={200} height={200} src={userData.image}/> */}
-          <p>{`Residential Address: ${userData?.address.address} ${userData?.address.city}`}</p>
-        </>
-      ) : (
-        <p>Loading user data...</p>
-      )}
+      <h2>{data.firstName} {data.lastName}</h2>
+      <p>{`Residential Address: ${data.address.address}, ${data.address.city}`}</p>
     </div>
   );
 }
