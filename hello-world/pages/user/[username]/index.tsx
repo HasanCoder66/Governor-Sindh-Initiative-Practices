18aa80c518a54e3cb734c49ba6ba76762e164113
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-23 16:37:44 +0500

    in this time i understand next router like reload, replace, push

--- hello-world/pages/user/[username]/index.tsx
+++ hello-world/pages/user/[username]/index.tsx
@@ -2,18 +2,17 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
 const index = () => {
-
-    const ifUser = () => ({user: null, loading:false})
-    const user = ifUser()
+  const ifUser = () => ({ user: true, loading: false });
+  const user = ifUser();
   const router = useRouter();
   const { query } = router;
 
-//   console.log(query, "userRouter Result===>");
-useEffect(()=> {
-if(user.user == null) {
-    router.replace('/')
-} 
-},[user.user, user])
+  //   console.log(query, "userRouter Result===>");
+  useEffect(() => {
+    if (user.user == null) {
+      router.replace("/");
+    }
+  }, [user.user, user]);
   return (
     <>
       <div>this is the index page for {query.username}</div>
@@ -36,6 +35,12 @@ if(user.user == null) {
         onClick={(e) => router.replace(`/`)}
       >
         Go to home page
+      </button>
+      <button
+        className="bg-white text-black block mb-[10px] p-[5px]"
+        onClick={(e) => router.reload()}
+      >
+        Reload
       </button>
     </>
   );
