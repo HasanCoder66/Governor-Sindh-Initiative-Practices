78d17b39f64590860e1d196c0ed5801c93937692
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-23 13:08:50 +0500

    practice for nav in next

---
+++ hello-world/pages/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='w-[100%] h-[90px] bg-[#123456]'>
+            <ul className='flex justify-between w-[80%] mx-auto  font-bold uppercase items-center pt-[35px]'>
+                <Link href="/">
+                <li>Home</li>
+                </Link>
+                
+                <Link href="/about">
+                
+                <li>About</li>
+                </Link>
+
+                <Link href='/services'>
+                <li>Services</li>
+                
+                </Link>
+                <Link href="/contact">
+                <li>Contact</li>
+                
+                </Link>
+                <Link href="/projects">
+                <li>Portfolio Projects</li>
+                
+                </Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
