6ee84cf1b9cea5a71604e85e959f23b15caba9a5
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-10-03 19:29:54 +0500

    global css testing

--- new-project/pages/static/users/index.jsx
+++ new-project/pages/static/users/index.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import styles from "../../../styles/Home.module.css";
 const usersPage = (props) => {
-    // console.log(props)
+  // console.log(props)
   return (
-    <div><h1>
-        Static users Page
-    </h1>
-         {props.data.users.map((item)=> <li key={item?.id}>{item.firstName}</li>)}
+    <div>
+      <h1>Static users Page</h1>
+      {props.data.users.map((item) => (
+        <li className={`${styles.text-yellow}`} key={item?.id}>{item.firstName}</li>
+      ))}
     </div>
-
-  )
-}
-
-
+  );
+};
 
 export const getStaticProps = async () => {
-    const data = await (await fetch('https://dummyjson.com/users')).json()
-    console.log('response from api', data)
-    return {
-        props: {
-            data 
-        }
-    }
-}
-export default usersPage
+  const data = await (await fetch("https://dummyjson.com/users")).json();
+  console.log("response from api", data);
+  return {
+    props: {
+      data,
+    },
+  };
+};
+export default usersPage;
